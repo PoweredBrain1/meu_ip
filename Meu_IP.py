e509801076e4af5b348f697f6bf646c9afd3fcfc
Nuno Barata Alves <11076303@edu.ulusiada.pt>
2022-01-19 18:32:23 +0000

    Add files via upload

---
+++ Meu_IP.py
@@ -0,0 +1,47 @@
+#Meu IP por Nuno Alves
+import socket
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+root = Tk()
+root.title("Meu IP Versão 0.2 de Nuno Barata Alves")
+
+h_name = socket.gethostname()
+IP_addres = socket.gethostbyname(h_name)
+nome = "O nome deste computador é: " + h_name
+ip = "O endereço IP deste computador é: " + IP_addres
+
+
+def time():
+    string = strftime('%H:%M:%S')
+    label.config(text=string)
+    label.after(1000, time)
+
+def nome1():
+    label1.config(text=nome)
+    label1.after(1000,nome1)
+
+def ip1():
+    label2.config(text=ip)
+    label2.after(1000,ip1)
+
+label = Label(root, font=("arial", 20), background = "white", foreground = "green")
+label1 = Label(root, font=("arial", 20), background = "white", foreground = "green")
+label2 = Label(root, font=("arial", 20), background = "white", foreground = "green")
+label1.pack(anchor='center')
+label2.pack(anchor='center')
+label.pack(anchor='center')
+
+
+nome1()
+ip1()
+time()
+
+
+
+fim = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nMeu IP Versão 0.1 de Nuno Alves.                       Prima enter para terminar"
+
+
+#print(input(fim))
+
+mainloop()
